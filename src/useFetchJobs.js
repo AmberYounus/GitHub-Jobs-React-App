380e6ea81bbb963a5bfedb37fc518a97ca66e070
Amber Younus <amberyounus22@gmail.com>
2023-12-15 23:43:57 -0800

    commit

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,32 +1,41 @@
-import { useReducer } from "react"
+import axios from "axios"
+import { useEffect, useReducer } from "react"
 
-const ACTIONS={
-    MAKE_REQUEST:'make-request',
-    GET_DATA:'get-data',
-    ERROR:'error'
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
+const BASE_URL = 'https://jobs.github.com/positions.json';
+
+function reducer(state, action) {
+    //  action.payload.x
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: [] }
+        default:
+            return state
+    }
 }
 
-function reducer(state,action){
-  //  action.payload.x
-  switch(action.type){
-    case ACTIONS.MAKE_REQUEST:
-        return {loading:true,jobs:[]}
-    case ACTIONS.GET_DATA:
-        return {...state,loading:false,jobs:action.payload.jobs}
-    case ACTIONS.ERROR:
-        return {...state,loading:false,error:action.payload.error,jobs:[]}
-    default:
-        return state
-  }
-}
 
+export default function useFetchJobs(params, page) {
+    const [state, dispatch] = useReducer(reducer, { jobs: [], loading: false })
 
-export default function useFetchJobs(params,page){
-    const[state,dispatch] =useReducer(reducer,{jobs:[],loading:false})
-    dispatch({type:'uuu',payload:{x:3}})
-return{
-    jobs:[],
-    loading:false,
-    error:false,
-}
+    useEffect(() => {
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            params: { markdown: true, page: page, ...params }
+        })
+    }, [params, page])
+
+    return {
+        jobs: [],
+        loading: false,
+        error: false,
+    }
 }
