5e73b9a8c5dd534de9c92d377c1d1359fd99f099
Amber Younus <amberyounus22@gmail.com>
2024-01-01 23:35:21 -0800

    commit

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 
-export default function SearchForm(){
-    
+export default function SearchForm(onParamChange){
+  <Form >
+    <Form.Row>
+        <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" name="description" value={description} onChange={onParamChange}/>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Location</Form.Label>
+            <Form.Control type="text" name="description" value={location} onChange={onParamChange}/>
+        </Form.Group>
+        <Form.Group>
+            <Form.Check type="checkbox" name="full_time" id="full-time" value={full_time} label="Only Full time" onChange={onParamChange}/>
+        </Form.Group>
+    </Form.Row>
+  </Form>
 }
