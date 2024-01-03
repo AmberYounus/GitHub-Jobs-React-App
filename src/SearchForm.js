f2cf28c2ada9b42ffa975ac23194214ba4cdbf42
Amber Younus <amberyounus22@gmail.com>
2024-01-02 23:27:20 -0800

    commit

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form,Col } from "react-bootstrap";
 
-export default function SearchForm(onParamChange){
-  <Form >
-    <Form.Row>
-        <Form.Group>
+export default function SearchForm({onParamChange,params}){
+  <Form className="mb-4">
+    <Form.Row className="align-items-end">
+        <Form.Group as={Col}>
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" name="description" value={description} onChange={onParamChange}/>
+            <Form.Control type="text" name="description" value={params.description} onChange={onParamChange}/>
         </Form.Group>
-        <Form.Group>
+        <Form.Group as={Col}>
             <Form.Label>Location</Form.Label>
-            <Form.Control type="text" name="description" value={location} onChange={onParamChange}/>
+            <Form.Control  type="text" name="location" value={params.location} onChange={onParamChange}/>
         </Form.Group>
-        <Form.Group>
-            <Form.Check type="checkbox" name="full_time" id="full-time" value={full_time} label="Only Full time" onChange={onParamChange}/>
+        <Form.Group as={Col} className="ml-2">
+            <Form.Check type="checkbox" className="mb-2" name="full_time" id="full-time" value={params.full_time} label="Only Full time" onChange={onParamChange}/>
         </Form.Group>
     </Form.Row>
   </Form>
