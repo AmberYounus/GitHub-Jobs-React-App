c8cc5589c5a30837c3a0755c3adead2453d89e54
Amber Younus <amberyounus22@gmail.com>
2024-01-05 23:09:12 -0800

    commit

--- src/JobsPagination.js
+++ src/JobsPagination.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Pagination } from "react-bootstrap";
 
-export default function JobsPagination({ page, setPage }) {
+export default function JobsPagination({ page, setPage,hasNextPage }) {
     function addJustPage(amount) {
         setPage(prevPage => prevPage + amount)
     }
@@ -13,6 +13,9 @@ export default function JobsPagination({ page, setPage }) {
             {page > 2 && <Pagination.Ellipsis />}
             {page > 2 && <Pagination.Item onClick={() => addJustPage(-1)}>{page - 1}</Pagination.Item>}
             <Pagination.Item active>{page}</Pagination.Item>
+            {hasNextPage && <Pagination.Item onClick={() => addJustPage(1)}>{page + 1}</Pagination.Item>}
+            {hasNextPage && <Pagination.Next onClick={() => addJustPage(1)}/>}
+
         </Pagination>
     )
 }
