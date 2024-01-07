91647c93a5dd86233cd2460c61a5ae9fa39008c8
Amber Younus <amberyounus22@gmail.com>
2024-01-06 23:20:48 -0800

    commit

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Badge, Card } from "react-bootstrap";
 
 export default function Job({job}) {
     return (
@@ -11,7 +11,12 @@ export default function Job({job}) {
                             <Card.Title>
                                 {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
                             </Card.Title>
-                            <Card.Subtitle className="text-muted"></Card.Subtitle>
+                            <Card.Subtitle className="text-muted mb-2">
+                                {new Date(job.created_at).toLocaleDateString()}
+                            </Card.Subtitle>
+                            <Badge className="mr-2" variant="secondary">{job.type}</Badge>
+                            <Badge className="text-dark" variant="secondary">{job.location}</Badge>
+                        
                         </div>
                     </div>
                 </Card.Body>
