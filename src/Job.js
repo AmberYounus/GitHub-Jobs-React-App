b09d9df8e2be609edf7377228c114db8c0b13fdd
Amber Younus <amberyounus22@gmail.com>
2024-01-07 23:49:55 -0800

    commit

--- src/Job.js
+++ src/Job.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { Badge, Card } from "react-bootstrap";
+import { Badge, Button, Card } from "react-bootstrap";
+import ReactMarkdown from "react";
 
-export default function Job({job}) {
+export default function Job({ job }) {
     return (
-        
-            <Card>
-                <Card.Body>
-                    <div className="d-flex justify-content-between">
-                        <div>
-                            <Card.Title>
-                                {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
-                            </Card.Title>
-                            <Card.Subtitle className="text-muted mb-2">
-                                {new Date(job.created_at).toLocaleDateString()}
-                            </Card.Subtitle>
-                            <Badge className="mr-2" variant="secondary">{job.type}</Badge>
-                            <Badge className="text-dark" variant="secondary">{job.location}</Badge>
-                        
+
+        <Card>
+            <Card.Body>
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <Card.Title>
+                            {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted mb-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge className="mr-2" variant="secondary">{job.type}</Badge>
+                        <Badge className="text-dark" variant="secondary">{job.location}</Badge>
+                        <div style={{ wordBreak: 'break-all' }}>
+                            <ReactMarkdown source={job.how_to_apply} />
                         </div>
+
                     </div>
-                </Card.Body>
-            </Card>
-        
+                    <img className="d-none d-md-block" alt={job.company} src={job} />
+                </div>
+                <Card.Text><Button variant="primary" onClick={() => { }}></Button></Card.Text>
+            </Card.Body>
+        </Card>
+
     )
 }
