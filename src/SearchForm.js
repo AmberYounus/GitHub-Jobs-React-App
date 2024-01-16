9521652f4716e1033b3cacca42f6a8ccb25eb8c7
Amber Younus <amberyounus22@gmail.com>
2024-01-15 23:39:37 -0800

    commit

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { Form,Col } from "react-bootstrap";
+import { Form, Col } from "react-bootstrap";
 
-export default function SearchForm({onParamChange,params}){
-  <Form className="mb-4">
-    <Form.Row className="align-items-end">
+export default function SearchForm({onParamChange, params}) {
+  return (
+    <Form className="mb-4">
+      <Form.Row className="align-items-end">
+
         <Form.Group as={Col}>
-            <Form.Label>Description</Form.Label>
-            <Form.Control type="text" name="description" value={params.description} onChange={onParamChange}/>
+          <Form.Label>Description</Form.Label>
+          <Form.Control type="text" name="description" id="description" value={params.description} onChange={onParamChange} />
         </Form.Group>
+
         <Form.Group as={Col}>
-            <Form.Label>Location</Form.Label>
-            <Form.Control  type="text" name="location" value={params.location} onChange={onParamChange}/>
+          <Form.Label>Location</Form.Label>
+          <Form.Control type="text" name="location" id="location" value={params.location} onChange={onParamChange} />
         </Form.Group>
+
         <Form.Group as={Col} xs="auto" className="ml-2">
-            <Form.Check type="checkbox" className="mb-2" name="full_time" id="full-time" value={params.full_time} label="Only Full time" onChange={onParamChange}/>
+          <Form.Check type="checkbox" className="mb-2" name="full_time" id="full-time" value={params.full_time} label="Only Full time" onChange={onParamChange} />
         </Form.Group>
-    </Form.Row>
-  </Form>
+
+      </Form.Row>
+    </Form>
+  )
 }
