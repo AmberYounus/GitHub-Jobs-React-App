13afcd22953bb9edf09d60795d82b08a6dcda231
Amber Younus <amberyounus22@gmail.com>
2024-01-20 22:50:37 -0800

    commit

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,33 +1,23 @@
 
 import axios from "axios";
-import { useEffect, useReducer } from "react"
+import { useEffect, useReducer } from "react";
 
 const ACTIONS = {
     MAKE_REQUEST: 'make-request',
     GET_DATA: 'get-data',
     ERROR: 'error',
-    UPDATE_HAS_NEXT_PAGE:'update-has-next-page '
+    UPDATE_HAS_NEXT_PAGE: 'update-has-next-page '
 }
-let url1 = 'https://jobs.github.com/positions.json'
-let data = {
-  description: 'javascript',
-  location: 'san francisco',
-}
-
-let params = Object.keys(data).map(key => key + '=' + encodeURIComponent(data[key])).join('&');
-let fullUrl = `${url1}?${params}`;
 
 //github jobs API 
 //Missing required request header. Must specify one of: origin,x-requested-with
-const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
-const url = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
-
-// const url = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python&location=new+york";
+ const url = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
+//  const url = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python&location=new+york";
 
 fetch(url)
-.then(response => response.json())
-.then(data => console.log("data "+data))
-.catch(err => console.log("error"+err.name))
+    .then(response => response.json())
+    .then(data => console.log("data " + data))
+    .catch(err => console.log("error" + err.name))
 
 function reducer(state, action) {
     //  action.payload.x
@@ -39,7 +29,7 @@ function reducer(state, action) {
         case ACTIONS.ERROR:
             return { ...state, loading: false, error: action.payload.error, jobs: [] }
         case ACTIONS.UPDATE_HAS_NEXT_PAGE:
-            return { ...state,hasNextPage:action.payload.hasNextPage }
+            return { ...state, hasNextPage: action.payload.hasNextPage }
         default:
             return state;
     }
@@ -50,28 +40,30 @@ export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
     useEffect(() => {
-        const cancelToken1 =axios.CancelToken.source();
+
+        const cancelToken1 = axios.CancelToken.source();
         dispatch({ type: ACTIONS.MAKE_REQUEST })
-        axios.get(BASE_URL, {
-            cancelToken:cancelToken1.token,
+        axios.get(url, {
+            cancelToken: cancelToken1.token,
             params: { markdown: true, page: page, ...params }
         }).then(res => {
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
         }).catch(e => {
-            if(axios.isCancel(e)) return
+            if (axios.isCancel(e)) return
             dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
         })
-        const cancelToken2 =axios.CancelToken.source();
-        axios.get(BASE_URL, {
-            cancelToken:cancelToken2.token,
-            params: { markdown: true, page: page+1, ...params }
+        
+        const cancelToken2 = axios.CancelToken.source();
+        axios.get(url, {
+            cancelToken: cancelToken2.token,
+            params: { markdown: true, page: page + 1, ...params }
         }).then(res => {
-            dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage:res.data.length !==0 } })
+            dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } })
         }).catch(e => {
-            if(axios.isCancel(e)) return
+            if (axios.isCancel(e)) return
             dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
         })
-        return()=>{
+        return () => {
             cancelToken1.cancel()
             cancelToken2.cancel()
 
